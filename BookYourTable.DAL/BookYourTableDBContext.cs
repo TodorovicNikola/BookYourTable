e7a6aca03719b7bf7f8ba66873e011a57b2e6c2a
nkl58 <nikola58tod@gmail.com>
2016-02-01 15:52:59 +0100

    Initial project created

---
+++ BookYourTable.DAL/BookYourTableDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace BookYourTable.DAL
+{
+    public class BookYourTableDBContext : DbContext
+    {
+        public BookYourTableDBContext() : base("BookYourTable")
+        {
+
+        }
+    }
+}
---
+++ BookYourTable/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookYourTable.MvcApplication" Language="C#" %>
